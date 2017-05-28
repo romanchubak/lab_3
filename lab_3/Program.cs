721e1d2580baf3c9f84114063930f3db261f5bfc
romanchubak <romanchubak@gmail.com>
2017-05-28 22:49:54 +0300

    init

--- lab_3/Program.cs
+++ lab_3/Program.cs
@@ -10,6 +10,30 @@ namespace lab_3
     {
         static void Main(string[] args)
         {
+            State E = new State(true);
+            State GD = new State(false);
+            State F = new State(true);
+            State H = new State(false);
+            State BC = new State { N = E, K = F, M = null, Finish = true };
+            State A = new State { N = BC, K = BC, M = GD, Finish = false };
+            GD.N = E;
+            GD.K = F;
+            F.K = H;
+            F.N = GD;
+            H.N = GD;
+            H.K = H;
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Input your string(q -> exit): ");
+                string s = Console.ReadLine();
+                if (s == "q")
+                    exit = true;
+                else
+                    Console.WriteLine(A.Check(s));
+            }
+            //Console.ReadKey();
         }
     }
 }
---
+++ lab_3/State.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    class State
+    {
+        public State N { get; set; }
+        public State K { get; set; }
+        public State M { get; set; }
+        public bool Finish { get; set; }
+
+        public State()
+        {
+            N = K = M =  null;
+            Finish = false;
+        }
+        public State(bool fin)
+        {
+            N = K = M = null;
+            Finish = fin;
+        }
+        public bool Check(string s)
+        {
+            if( s != "")
+                switch (s[0])
+                {
+                    case 'n': return N != null && N.Check(s.Substring(1));
+                    case 'k': return K != null && K.Check(s.Substring(1));
+                    case 'm': return M != null && M.Check(s.Substring(1));
+                }
+            return Finish;
+        }
+        
+    }
+}
